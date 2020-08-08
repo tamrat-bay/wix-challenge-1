54d9b9c75f5336f031028a009aab5a91a49e69cf
tamrat-bayeh <tamrat3@gmail.com>
2020-08-08 10:14:30 +0300

    created initial components / added M-UI / Grid Layout / CarCard initial design

---
+++ server/src/index.ts
@@ -0,0 +1,3 @@
+import express, { Application, Request, Response } from "express";
+import axios from 'axios';
+import { ICars } from './models/carsModel'
---
+++ server/src/models/carsModel.ts
@@ -0,0 +1,10 @@
+export interface ICars {
+  id: number;
+  car: string;
+  car_model: string;
+  car_color: string;
+  car_model_year: number;
+  car_vin: string;
+  price: string;
+  availability: Boolean;
+}
