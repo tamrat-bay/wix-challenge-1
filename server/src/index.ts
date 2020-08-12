ce32799f1e1fac709858004251ad6c8cb8331804
tamrat-bayeh <tamrat3@gmail.com>
2020-08-12 14:54:44 +0300

    added route dir and deleted handlers dir / server refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import { config } from 'dotenv'
 import mongoose from 'mongoose';
-import { getCars, addCar, deleteCar, editCar } from './handlers/carHandler'
+// import { getCars, addCar, deleteCar, editCar } from './handlers/carHandler';
+import carsRouter from './routes/cars'
 
 const app: Application = express();
 const Port: number | string = process.env.PORT || 5000;
@@ -11,29 +12,15 @@ config()
 
 const mongoURL: string = process.env.DB_URL || "localhost";
 
-mongoose.connect(mongoURL, {
+mongoose.connect(mongoURL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
   .then(() => console.log("MongoDb is Connected"))
   .catch((err) => console.log(err));
+
+app.use('/cars', carsRouter )
+
   
-  
-app.get("/cars", (req: Request, res: Response) => {
-  return getCars(req, res)
-});
-
-app.post("/cars", (req: Request, res: Response) => {
-  return addCar(req, res)
-});
-
-app.delete("/cars/:id", (req: Request, res: Response) => {
-  return deleteCar(req, res)
-});
-
-app.put("/cars/:id", (req: Request, res: Response) => {
-  return editCar(req, res)
-});
-
 app.listen(Port, () => console.log(`Server is listening on port ${Port}`));
