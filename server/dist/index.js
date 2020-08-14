e4c701d669754294e8212083b718af82a69e37b8
tamrat-bayeh <tamrat3@gmail.com>
2020-08-14 11:38:47 +0300

    heroku upload

---
+++ server/dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const path_1 = __importDefault(require("path"));
+const carsRouter_1 = __importDefault(require("./routes/carsRouter"));
+const app = express_1.default();
+const Port = process.env.PORT || 5000;
+app.use(express_1.default.json());
+const mongoURL = "mongodb+srv://tamrat:87654321@mybudget-ajn0y.mongodb.net/wix-cars?retryWrites=true&w=majority";
+mongoose_1.default.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+    .then(() => console.log("MongoDB is Connected"))
+    .catch((err) => console.log(err));
+app.use('/cars', carsRouter_1.default);
+if (process.env.NODE_ENV === "production") {
+    const buildPath = path_1.default.join(__dirname, "..", "..", "client", "build");
+    app.use(express_1.default.static(buildPath));
+    // --- handle unknown route
+    app.get("*", (req, res) => {
+        const indexHtmlPath = path_1.default.join(buildPath, "index.html");
+        res.sendFile(indexHtmlPath);
+    });
+}
+else {
+    console.log("Development mode");
+}
+app.listen(Port, () => console.log(`Server is listening on port ${Port}`));
---
+++ server/dist/models/carsSchema.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const carSchema = new mongoose_1.default.Schema({
+    id: Number,
+    car: {
+        type: String,
+        required: true,
+    },
+    car_model: {
+        type: String,
+        required: true,
+    },
+    car_color: String,
+    car_model_year: {
+        type: Number,
+        required: true,
+    },
+    car_vin: {
+        type: String,
+        required: false,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    availability: Boolean,
+    img: {
+        type: String,
+        required: true,
+    },
+});
+const Cars = mongoose_1.default.model("cars", carSchema);
+exports.default = Cars;
