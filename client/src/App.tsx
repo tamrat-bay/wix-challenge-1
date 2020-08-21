88032d91139c47d2a98891e85c7045a18741c888
tamrat-bayeh <tamrat3@gmail.com>
2020-08-22 00:24:28 +0300

    heroku upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,9 @@ import CarsBoard from "./components/CarsBoard/CarsBoard";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Authentication/Login/Login";
 import SignUp from "./components/Authentication/SignUp/SignUp";
-import Home from "./components/Home/Home";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import "./App.css";
 
 const App: React.FC = () => {
