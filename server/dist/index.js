7ec340611aa8d0562ba7a714f46a225cb4a987fd
tamrat-bayeh <tamrat3@gmail.com>
2020-08-24 18:45:05 +0300

    heroku upload

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,14 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const path_1 = __importDefault(require("path"));
-const dotenv_1 = require("dotenv");
 const carsRouter_1 = __importDefault(require("./routes/carsRouter"));
 const usersRouter_1 = __importDefault(require("./routes/usersRouter"));
 const app = express_1.default();
 const Port = process.env.PORT || 5000;
 app.use(express_1.default.json());
-dotenv_1.config();
-const mongoURL = process.env.DB_URL || "localhost";
+const mongoURL = "mongodb+srv://tamrat:87654321@mybudget-ajn0y.mongodb.net/wix-cars?retryWrites=true&w=majority";
 mongoose_1.default
     .connect(mongoURL, {
     useNewUrlParser: true,
